499fa17720e89ed914720130b22c5862c817de94
saeedane <dvdplay405@gmail.com>
2022-08-03 10:59:48 +0100

    custom file jkavascript

--- js/ajax-javascript.js
+++ js/ajax-javascript.js
@@ -129,3 +129,24 @@ jQuery(document).ready(function(){
             });
     });   
     
+/**
+ * upload image category 
+ */    
+
+ jQuery(function($){
+    $('body').on('click', '.alhassade_upload_image_button', function(e){
+
+      e.preventDefault();
+        aw_uploader = wp.media({
+            title: 'Custom image',
+            button: {
+                text: 'Use this image'
+            },
+            multiple: false
+        }).on('select', function() {
+            var attachment = aw_uploader.state().get('selection').first().toJSON();
+            $('#cat-image').val(attachment.url);
+        })
+        .open();
+    });
+});
