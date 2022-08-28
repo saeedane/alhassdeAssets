ca3b1f4e3f5a3d37ef9ef4a511c930eb227eff70
saeedane <dvdplay405@gmail.com>
2022-08-29 00:10:47 +0100

    update file script

--- js/scripts.js
+++ js/scripts.js
@@ -166,6 +166,8 @@ function switchTweetTheme(currentTheme,targetTheme) {
                     $(".sticky .header__logo img").attr('class','mb-3 mt-2');
                     $(".sticky .header__social .nav  .nav-item .nav-link .sticky-icon").removeClass('d-none');
                     $(".sticky .header__social .nav  .nav-item .nav-link .simple-icon").addClass('d-none');
+                    $(" .header__left sticky-icon").removeClass('d-none');
+                    $(" .header__left .simple-icon").addClass('d-none');
 
                  }else{
                     $(".header_area").removeClass("sticky");
@@ -173,6 +175,9 @@ function switchTweetTheme(currentTheme,targetTheme) {
                     $(".header__logo img").attr('class','mb-0');
                     $(" .header__social .nav  .nav-item .nav-link .sticky-icon").addClass('d-none');
                     $(" .header__social .nav  .nav-item .nav-link .simple-icon").removeClass('d-none');
+                    $(" .header__left sticky-icon").addClass('d-none');
+                    $(" .header__left .simple-icon").removeClass('d-none');
+
 
                  }
 
